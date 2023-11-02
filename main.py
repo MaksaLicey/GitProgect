0c798c5094d965de0b67bf5ec8ed56d985a17563
maksa <maksazzw@gmail.com>
2023-11-02 16:21:20 +0900

    4 version

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def print_main():
-    print("DSD@fg")
+import sys
 
-    print('hkhkhkhhkhkkhkkkhkkkhh')
-#sret
-print_main()
+
+def my_superfunction():
+    print('What an !!!!!!!!!!!!!!!!!!!!!!!!!!! print!')
+
+
+def main():
+    print('My first git program')
+    print('And I change it every day')
+    print('Again')
+    print('UFO came and added this line')
+    my_superfunction()
+
+
+if __name__ == '__main__':
+    main()
